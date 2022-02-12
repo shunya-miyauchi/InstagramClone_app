6ed747dc06ae4f7370a12c6f11301d153ab07fa4
Miyauchi <hy.miyauchi@gmail.com>
2022-02-12 12:11:18 +0900

    update model/user validates設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 30}
+  validates :user_name,presence :true, length: {maximum:10}
+  validates :email,presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
