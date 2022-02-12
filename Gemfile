19f29cbafcb00fd2c9fd278e9d46e67a46b3886b
Miyauchi <hy.miyauchi@gmail.com>
2022-02-12 16:09:20 +0900

    install 'bcrypt' & upadate model/user validates

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 # add
 gem 'psych' , '~> 3.1'
+gem 'bcrypt'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   validates :user_name,presence :true, length: {maximum:10}
   validates :email,presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password,length: {minimum:6}
+
+  before_validation {email.downcase!}
+  has_secure_password
 end
