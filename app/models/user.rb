3fcf068429384c07b86cf3f06223613b63df5539
Miyauchi <hy.miyauchi@gmail.com>
2022-02-12 16:18:36 +0900

    modify validates/user_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
-  validates :user_name,presence :true, length: {maximum:10}
+  validates :user_name, presence: true, length: {maximum:10}
   validates :email,presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password,length: {minimum:6}
 
